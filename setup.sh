4c603d283f59c62e6b8f2c40d7066eab9d133ea3
Jose Ulloa <jandres@hotmail.com>
2021-07-10 17:13:24 -0600

    test

--- setup.sh
+++ setup.sh
@@ -5,12 +5,14 @@ set -e
 rm -rf ~/.vim
 rm -f ~/.vimrc
 rm -f ~/.bashrc
+rm -f ~/.tmux.conf
 rm -f ~/.config/nvim/init.vim
 
 # create symlinks
 ln -s .vim ~/.vim
 ln -s .vimrc ~/.vimrc
 ln -s .bashrc ~/.bashrc
+ln -s .tmux.conf ~/.tmux.conf
 ln -s .nvim/init.vim ~/.config/nvim/init.vim
 
 # install plugins
